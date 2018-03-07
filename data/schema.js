b9088a8e0391fcdd5bdda3375c3044b7e0aa77af
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-03-07 08:51:51 +0000

    feat: Add node graphql schema

---
+++ data/schema.js
@@ -0,0 +1,95 @@
+const { buildSchema } = require('graphql')
+
+const Schema = (buildSchemaGQL = buildSchema) => buildSchemaGQL(`
+  #
+  # Types, Enums, Interfaces
+  #
+
+  type User {
+    id: ID!
+    email: String!
+    password_hash: String!
+    status: AccountStatus
+    name: String
+    role: Role
+  }
+
+  type Profile {
+    email: String!
+    status: AccountStatus
+    name: String
+    role: Role
+  }
+
+  type Credential {
+    username: String!
+    password: String!
+  }
+
+  type Token {
+    jwt: String!
+  }
+
+  enum AccountStatus {
+    AWAITING_CONFIRMATION
+    REGISTERED
+    CLOSED
+    DISABLED
+  }
+
+  enum Role {
+    ADMIN
+    CONSUMER
+  }
+
+  type Opportunity {
+    id: ID!
+    company: String!
+    address: String
+    money: Int
+    recruiter: Recruiter
+    note: Note // @relationship
+  }
+
+  type Recruiter {
+    id: ID!
+    name: String
+    telephone: String
+  }
+
+  type Note {
+    id: ID!
+    comments: String! // to be read in markdown
+  }
+
+  type Query {
+    login(credential: Credential): Token
+    getMyProfile: Profile
+    getOpportunities: [Opportunity]
+  }
+
+  #
+  # Mutations
+  #
+  type Mutation {
+    register(credential: Credential!): String! // email address
+    confirmAccount(confirmationCode: String!):
+    createOpportunity(opportunity: Opportunity!): Opportunity!
+    updateOpportunity(opportunity: Opportunity!): Opportunity!
+    deleteOpportunity(id: Int!): Int!
+  }
+
+  #
+  # Subscriptions
+  #
+  type Subscription {
+
+  }
+`)
+
+const schema = Schema()
+
+module.exports = {
+  Schema,
+  schema
+}
