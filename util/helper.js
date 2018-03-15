865d1c10c7416ec3dce230030e57ca7c96072d7d
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-03-15 08:52:42 +0000

    feat: Extract query helpers

---
+++ util/helper.js
@@ -0,0 +1,14 @@
+const getNowISO = () => {
+  const d = new Date()
+  return d.toISOString()
+}
+
+const isValidEmail = email => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(email)
+}
+
+module.exports = {
+  getNowISO,
+  isValidEmail
+}
