5ddd77948facf91e80cba561deff6c70930048c3
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-03-21 23:56:17 +0000

    feat: Add helper to throw on malformed credential

--- util/helper.js
+++ util/helper.js
@@ -10,8 +10,25 @@ const isValidEmail = email => {
 
 const generateRandomAlphanumericString = (length = 10) => Math.random().toString(36).slice(length * -1);
 
+/**
+ * Throws an error if credential malformed
+ * @param {Object} credential: { email: String, password: String }
+ */
+const validateCredential = credential => {
+  if (!credential || !credential.email || !credential.password) {
+    throw 'Missing args'
+  }
+  if (typeof credential.email !== 'string' || typeof credential.password !== 'string') {
+    throw new Error('Invalid arg type(s)')
+  }
+  if (!isValidEmail(credential.email)) {
+    throw 'Invalid email'
+  }
+}
+
 module.exports = {
   getNowISO,
   isValidEmail,
-  generateRandomAlphanumericString
+  generateRandomAlphanumericString,
+  validateCredential
 }
