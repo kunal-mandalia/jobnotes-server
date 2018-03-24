5e5c749ca48617c74d7277c2dbf24bf15b7ca9d7
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-03-24 13:35:25 +0000

    feat: Add instance methods to login, setPassword

--- data/model.js
+++ data/model.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const { sequelize } = require('./connection')
 const constant = require('./constants')
+const { getNowISO } = require('../util/helper')
 
 const User = sequelize.define('users', {
   user_id: { type: Sequelize.INTEGER, unique: true, primaryKey: true, autoIncrement: true },
@@ -17,19 +20,26 @@ const User = sequelize.define('users', {
     constant.CLOSED,
     constant.DISABLED,
     constant.REGISTERED
-  )
-}, {
-  // As per https://stackoverflow.com/questions/34120548/using-bcrypt-with-sequelize-model
-  freezeTableName: true,
-  instanceMethods: {
-    generateHash(password) {
-      return bcrypt.hash(password, bcrypt.genSaltSync(8));
-    },
-    validPassword(password) {
-      return bcrypt.compare(password, this.password);
-    }
+  )}
+)
+
+// Instance methods
+User.prototype.setPassword = async function (password) {
+  this.password = await bcrypt.hash(password, bcrypt.genSaltSync(8))
+  await this.save()
+}
+
+User.prototype.login = async function (password) {
+  const isPasswordValid = await bcrypt.compare(password, this.password)
+  if (isPasswordValid) {
+    const token = await jwt.sign({ user_id: this.user_id }, process.env.JWT_SECRET_KEY)
+    this.last_login = getNowISO()
+    await this.save()
+    return token
+  } else {
+    throw 'Bad credential'
   }
-})
+}
 
 module.exports = {
   User
